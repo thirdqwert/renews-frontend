b0090ccc6a5f3eaa1d7f8be76aa9cb1771860547
abdulloh <abdulloh@enail.com>
2026-02-13 22:10:07 +0500

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,57 @@
-import Image from "next/image";
+interface INews {
+    id: number,
+    title: string,
+    category: string[]
+}
 
-export default function Home() {
+export const revalidate = 180
+
+export default async function Home() {
+  const res_login = await fetch("http://127.0.0.1:8000/login/",
+    {
+        method: 'post',
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({username: `${process.env.ADMIN_LOGIN}`, password: `${process.env.ADMIN_PASSWORD}`})
+    }
+  )
+
+  const data_login = await res_login.json()
+  const access = data_login.access
+
+  const res = await fetch("http://127.0.0.1:8000/news/",
+    {
+        method: 'get',
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${access}`
+        }
+    }
+  )
+
+  const news = await res.json()
+  const news_politics = news.filter((item: INews) => item.category.includes('погода'))
+
+
+
+
+  
+  console.log(news_politics)
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+    <div>
+        <h2>News</h2>
+        {news.map(( item: INews )=> (
+            <div key={item.id}>
+                <h2>{item.id} - {item.title}</h2>
+            </div>
+        ))}
+        <h2>Politcs</h2>
+        {news.map(( item: INews )=> (
+            <div key={item.id}>
+                <h2>{item.id} - {item.title}</h2>
+            </div>
+        ))}
     </div>
   );
 }
